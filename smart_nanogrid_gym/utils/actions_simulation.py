9ca85c357bbf88f077f4e5a151e7f03d9ea36c3c
AJ <asolic00@fesb.hr>
2023-02-01 14:46:23 +0100

    Fix: Resolve charging vehicle after it departed charger

--- smart_nanogrid_gym/utils/actions_simulation.py
+++ smart_nanogrid_gym/utils/actions_simulation.py
@@ -7,6 +7,7 @@ def simulate_central_management_system(self, actions):
     consumed = self.energy['Consumed']
     renewable = self.energy['Available renewable']
     charger_occupancy = self.initial_simulation_values['Charger occupancy']
+    arrivals = self.initial_simulation_values['Arrivals']
 
     departing_vehicles = self.departing_vehicles
     soc = self.ev_state_of_charge
@@ -19,7 +20,10 @@ def simulate_central_management_system(self, actions):
     for charger in range(self.NUMBER_OF_CHARGERS):
         if charger_occupancy[charger, hour] != 0:
             if actions[charger] >= 0:
-                max_charging_energy = min([10, (1-soc[charger, hour]) * self.EV_PARAMETERS['CAPACITY']])
+                if hour in arrivals[charger]:
+                    max_charging_energy = min([10, (1 - soc[charger, hour]) * self.EV_PARAMETERS['CAPACITY']])
+                else:
+                    max_charging_energy = min([10, (1 - soc[charger, hour-1]) * self.EV_PARAMETERS['CAPACITY']])
             else:
                 max_charging_energy = min([10, soc[charger, hour] * self.EV_PARAMETERS['CAPACITY']])
         else:
@@ -30,8 +34,10 @@ def simulate_central_management_system(self, actions):
     # ----------------------------------------------------------------------------
     # Calculation of next state of Battery based on actions
     for charger in range(self.NUMBER_OF_CHARGERS):
-        if charger_occupancy[charger, hour] == 1:
-            soc[charger, hour+1] = soc[charger, hour] + charging_power[charger]/self.EV_PARAMETERS['CAPACITY']
+        if charger_occupancy[charger, hour] == 1 and hour in arrivals[charger]:
+            soc[charger, hour] = soc[charger, hour] + charging_power[charger]/self.EV_PARAMETERS['CAPACITY']
+        elif charger_occupancy[charger, hour] == 1 and hour not in arrivals[charger]:
+            soc[charger, hour] = soc[charger, hour-1] + charging_power[charger]/self.EV_PARAMETERS['CAPACITY']
 
     # ----------------------------------------------------------------------------
     # Calculation of energy utilization from the PV
