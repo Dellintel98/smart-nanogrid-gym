ec4aaf251cbb2f4433696e0e8ba343cd9c92fdfc
AJ <asolic00@fesb.hr>
2023-02-06 18:21:30 +0100

    Refactor: Clean code to form functions that do only one thing

--- smart_nanogrid_gym/utils/actions_simulation.py
+++ smart_nanogrid_gym/utils/actions_simulation.py
@@ -2,6 +2,66 @@ import numpy as np
 import time
 
 
+def calculate_charging_or_discharging_power(max_charging_power, action):
+    return action * max_charging_power
+
+
+def calculate_next_vehicle_state_of_charge(power_value, self, arrival, hour, soc):
+    if hour in arrival:
+        soc[hour] = soc[hour] + power_value / self.EV_PARAMETERS['CAPACITY']
+    else:
+        soc[hour] = soc[hour - 1] + power_value / self.EV_PARAMETERS['CAPACITY']
+        # soc[charger, timestep] = soc[charger, timestep - 1] + (charging_power[charger] * time_interval) / \
+        #                          self.EV_PARAMETERS['CAPACITY']
+
+
+def calculate_max_charging_power(self, arrival, hour, soc):
+    # max_charging_energy = min([self.EV_PARAMETERS['MAX CHARGING POWER'],
+    #                           soc[charger, timestep] * self.EV_PARAMETERS['CAPACITY'] / time_interval])
+    if hour in arrival:
+        remaining_uncharged_capacity = 1 - soc[hour]
+        power_left_to_charge = remaining_uncharged_capacity * self.EV_PARAMETERS['CAPACITY']
+    else:
+        remaining_uncharged_capacity = 1 - soc[hour - 1]
+        power_left_to_charge = remaining_uncharged_capacity * self.EV_PARAMETERS['CAPACITY']
+
+    max_charging_energy = min([self.EV_PARAMETERS['MAX CHARGING POWER'], power_left_to_charge])
+    return max_charging_energy
+
+
+def charge_vehicle(self, action, arrival, hour, vehicle_soc):
+    max_charging_power = calculate_max_charging_power(self, arrival, hour, vehicle_soc)
+    charging_power = calculate_charging_or_discharging_power(max_charging_power, action)
+    calculate_next_vehicle_state_of_charge(charging_power, self, arrival, hour, vehicle_soc)
+    return charging_power
+
+
+def calculate_max_discharging_power(self, arrival, hour, soc):
+    if hour in arrival:
+        power_left_to_discharge = soc[hour] * self.EV_PARAMETERS['CAPACITY']
+    else:
+        power_left_to_discharge = soc[hour - 1] * self.EV_PARAMETERS['CAPACITY']
+
+    max_discharging_energy = min([self.EV_PARAMETERS['MAX DISCHARGING POWER'], power_left_to_discharge])
+    return max_discharging_energy
+
+
+def discharge_vehicle(self, action, arrival, hour, vehicle_soc):
+    max_discharging_power = calculate_max_discharging_power(self, arrival, hour, vehicle_soc)
+    discharging_power = calculate_charging_or_discharging_power(max_discharging_power, action)
+    calculate_next_vehicle_state_of_charge(discharging_power, self, arrival, hour, vehicle_soc)
+    return discharging_power
+
+
+def charge_or_discharge_vehicle(self, action, arrival, hour, vehicle_soc):
+    if action >= 0:
+        charger_power_value = charge_vehicle(self, action, arrival, hour, vehicle_soc)
+    else:
+        charger_power_value = discharge_vehicle(self, action, arrival, hour, vehicle_soc)
+
+    return charger_power_value
+
+
 def simulate_central_management_system(self, actions):
     # hour = self.timestep
     # timestep = self.timestep
@@ -15,43 +75,21 @@ def simulate_central_management_system(self, actions):
     departing_vehicles = self.departing_vehicles
     soc = self.ev_state_of_charge
 
-    charging_power = np.zeros(self.NUMBER_OF_CHARGERS)
+    charger_power_values = np.zeros(self.NUMBER_OF_CHARGERS)
 
-    # ----------------------------------------------------------------------------
-    # Calculation of demand based on actions
-    # Calculation of actions for cars
     for charger in range(self.NUMBER_OF_CHARGERS):
         # to-do later (maybe): -1=Charger reserved -> lasts for max 15 minutes, 1=Occupied, 0=Empty
-        if charger_occupancy[charger, hour] != 0:
-            if actions[charger] >= 0:
-                if hour in arrivals[charger]:
-                    max_charging_energy = min([self.EV_PARAMETERS['MAX CHARGING POWER'], (1 - soc[charger, hour]) * self.EV_PARAMETERS['CAPACITY']])
-                else:
-                    max_charging_energy = min([self.EV_PARAMETERS['MAX CHARGING POWER'], (1 - soc[charger, hour-1]) * self.EV_PARAMETERS['CAPACITY']])
-            else:
-                max_charging_energy = min([self.EV_PARAMETERS['MAX DISCHARGING POWER'], soc[charger, hour] * self.EV_PARAMETERS['CAPACITY']])
-                # max_charging_power = min([self.EV_PARAMETERS['MAX CHARGING POWER'],
-                #                           soc[charger, timestep] * self.EV_PARAMETERS['CAPACITY'] / time_interval])
+        if charger_occupancy[charger, hour] == 1:
+            charger_power_values[charger] = charge_or_discharge_vehicle(self, actions[charger], arrivals[charger],
+                                                                        hour, soc[charger])
         else:
-            max_charging_energy = 0
-
-        charging_power[charger] = actions[charger]*max_charging_energy
-
-    # ----------------------------------------------------------------------------
-    # Calculation of next state of Battery based on actions
-    for charger in range(self.NUMBER_OF_CHARGERS):
-        if charger_occupancy[charger, hour] == 1 and hour in arrivals[charger]:
-            soc[charger, hour] = soc[charger, hour] + charging_power[charger]/self.EV_PARAMETERS['CAPACITY']
-        elif charger_occupancy[charger, hour] == 1 and hour not in arrivals[charger]:
-            soc[charger, hour] = soc[charger, hour-1] + charging_power[charger]/self.EV_PARAMETERS['CAPACITY']
-            # soc[charger, timestep] = soc[charger, timestep - 1] + (charging_power[charger] * time_interval) / \
-            #                          self.EV_PARAMETERS['CAPACITY']
+            charger_power_values[charger] = 0
 
     # ----------------------------------------------------------------------------
     # Calculation of energy utilization from the PV
     # Calculation of energy coming from Grid
     available_renewable_energy = max([0, renewable[0, hour] - consumed[0, hour]])
-    total_charging_power = sum(charging_power)
+    total_charging_power = sum(charger_power_values)
 
     # ----------------------------------------------------------------------------
     # First Cost index
