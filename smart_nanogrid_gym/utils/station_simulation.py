ddd66635029bd657f424d0f9188f4fc3424c60b9
AJ <asolic00@fesb.hr>
2023-02-07 13:05:06 +0100

    Refactor: Clean simulate_ev_charging_station function
    Description:
    simulate_ev_charging_station was divided into several sections.
    That means it did more than one thing. Complex functions that
    do more than one thing often contain bugs and are more difficult
    to refactor or adjust to new functionalities.
    
    Extract sections of simulate_ev_charging_station into new
    'do one thing' functions.
    
    simulate_ev_charging_station now does one thing.

--- smart_nanogrid_gym/utils/station_simulation.py
+++ smart_nanogrid_gym/utils/station_simulation.py
@@ -1,6 +1,63 @@
 import numpy as np
 
 
+def check_charger_occupancy(charger_occupancy):
+    if charger_occupancy == 1:
+        return True
+    else:
+        return False
+
+
+def check_is_vehicle_departing(vehicle_departure, hour):
+    if hour + 1 in vehicle_departure:
+        return True
+    else:
+        return False
+
+
+def find_departing_vehicles(number_of_chargers, hour, departures, charger_occupancy):
+    if hour >= 24:
+        return []
+
+    departing_vehicles = []
+    for charger in range(number_of_chargers):
+        charger_occupied = check_charger_occupancy(charger_occupancy[charger, hour])
+        vehicle_departing = check_is_vehicle_departing(departures[charger], hour)
+
+        if charger_occupied and vehicle_departing:
+            departing_vehicles.append(charger)
+
+    return departing_vehicles
+
+
+def calculate_next_departure_time(charger_departures, hour):
+    for vehicle in range(len(charger_departures)):
+        if hour <= charger_departures[vehicle]:
+            return charger_departures[vehicle] - hour
+    return []
+
+
+def calculate_departure_times(number_of_chargers, hour, departures, charger_occupancy):
+    departure_times = []
+    for charger in range(number_of_chargers):
+        charger_occupied = check_charger_occupancy(charger_occupancy[charger, hour])
+
+        if charger_occupied:
+            departure_time = calculate_next_departure_time(departures[charger], hour)
+            departure_times.append(departure_time)
+        else:
+            departure_times.append(0)
+
+    return departure_times
+
+
+def calculate_state_of_charge_for_each_vehicle(number_of_chargers, vehicle_soc, hour):
+    vehicles_state_of_charge = []
+    for charger in range(number_of_chargers):
+        vehicles_state_of_charge.append(vehicle_soc[charger, hour])
+    return vehicles_state_of_charge
+
+
 def simulate_ev_charging_station(self):
     vehicle_soc = self.ev_state_of_charge
     arrivals = self.initial_simulation_values['Arrivals']
@@ -10,28 +67,10 @@ def simulate_ev_charging_station(self):
     day = self.day
     hour = self.timestep
 
-    # calculation of which cars depart now
-    departing_vehicles = []
-    if hour < 24:
-        for charger in range(number_of_chargers):
-            departure_vehicle = departures[charger]
-            if charger_occupancy[charger, hour] == 1 and (hour+1 in departure_vehicle):
-                departing_vehicles.append(charger)
+    departing_vehicles = find_departing_vehicles(number_of_chargers, hour, departures, charger_occupancy)
 
-    # calculation of the hour each car is leaving
-    departure_times = []
-    for charger in range(number_of_chargers):
-        if charger_occupancy[charger, hour] == 0:
-            departure_times.append(0)
-        else:
-            for vehicle in range(len(departures[charger])):
-                if hour <= departures[charger][vehicle]:
-                    departure_times.append(departures[charger][vehicle]-hour)
-                    break
+    departure_times = calculate_departure_times(number_of_chargers, hour, departures, charger_occupancy)
 
-    # calculation of the BOC of each car
-    vehicles_state_of_charge = []
-    for charger in range(number_of_chargers):
-        vehicles_state_of_charge.append(vehicle_soc[charger, hour])
+    vehicles_state_of_charge = calculate_state_of_charge_for_each_vehicle(number_of_chargers, vehicle_soc, hour)
 
     return departing_vehicles, departure_times, vehicles_state_of_charge
