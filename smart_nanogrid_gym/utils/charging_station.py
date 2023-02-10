3de20286cd0e32ab5d4329f728d9c9e6a289e5d3
AJ <asolic00@fesb.hr>
2023-02-10 16:45:24 +0100

    Feat: Implement ChargingStation methods for handling initialisation

--- smart_nanogrid_gym/utils/charging_station.py
+++ smart_nanogrid_gym/utils/charging_station.py
@@ -1,3 +1,8 @@
+import numpy as np
+from numpy import random
+from scipy.io import loadmat, savemat
+
+
 class ChargingStation:
     def __init__(self, number_of_chargers):
         self.NUMBER_OF_CHARGERS = number_of_chargers
@@ -5,6 +10,7 @@ class ChargingStation:
         self.arrivals = None
         self.departures = None
         self.charger_occupancy = None
+        self.total_vehicles_charging = None
         self.departing_vehicles = []
         self.departure_times = []
         self.vehicle_state_of_charge_at_current_timestep = []
@@ -61,3 +67,95 @@ class ChargingStation:
         self.vehicle_state_of_charge_at_current_timestep.clear()
         for charger in range(self.NUMBER_OF_CHARGERS):
             self.vehicle_state_of_charge_at_current_timestep.append(self.vehicle_state_of_charge[charger, hour])
+
+    def load_initial_values(self, file_directory_path):
+        initial_values = loadmat(file_directory_path + '\\initial_values.mat')
+
+        arrival_times = initial_values['Arrivals']
+        departure_times = initial_values['Departures']
+
+        reformatted_arrivals = []
+        reformatted_departures = []
+
+        for charger in range(self.NUMBER_OF_CHARGERS):
+            if arrival_times.shape == (1, 10):
+                arrivals = arrival_times[0][charger][0]
+                departures = departure_times[0][charger][0]
+            elif arrival_times.shape == (10, 3):
+                arrivals = arrival_times[charger]
+                departures = departure_times[charger]
+            else:
+                raise Exception("Initial values loaded from initial_values.mat have wrong shape.")
+
+            reformatted_arrivals.append(arrivals.tolist())
+            reformatted_departures.append(departures.tolist())
+
+        self.vehicle_state_of_charge = initial_values['SOC']
+        self.arrivals = reformatted_arrivals
+        self.departures = reformatted_departures
+        self.charger_occupancy = initial_values['Charger occupancy']
+        self.total_vehicles_charging = initial_values['Total vehicles charging']
+
+    def generate_new_values(self, file_directory_path):
+        arrivals = []
+        departures = []
+
+        vehicle_state_of_charge = np.zeros([self.NUMBER_OF_CHARGERS, 25])
+        charger_occupancy = np.zeros([self.NUMBER_OF_CHARGERS, 25])
+
+        # initial state stochastic creation
+        for charger in range(self.NUMBER_OF_CHARGERS):
+            is_vehicle_present = 0
+            total_occupancy_timesteps_per_charger = 0
+            arrival = 0
+
+            vehicle_arrivals = []
+            vehicle_departures = []
+
+            for hour in range(24):
+                if is_vehicle_present == 0:
+                    arrival = round(random.rand() - 0.1)
+                    if arrival == 1 and hour <= 20:
+                        random_integer = random.randint(20, 50)
+                        vehicle_state_of_charge[charger, hour] = random_integer / 100
+
+                        total_occupancy_timesteps_per_charger = total_occupancy_timesteps_per_charger + 1
+
+                        vehicle_arrivals.append(hour)
+                        upper_limit = min(hour + 10, 25)
+                        vehicle_departures.append(random.randint(hour + 4, int(upper_limit)))
+
+                if arrival == 1 and total_occupancy_timesteps_per_charger > 0:
+                    if hour < vehicle_departures[total_occupancy_timesteps_per_charger - 1]:
+                        is_vehicle_present = 1
+                        charger_occupancy[charger, hour] = 1
+                    else:
+                        is_vehicle_present = 0
+                        charger_occupancy[charger, hour] = 0
+                else:
+                    is_vehicle_present = 0
+                    charger_occupancy[charger, hour] = 0
+
+            arrivals.append(vehicle_arrivals)
+            departures.append(vehicle_departures)
+
+        # information vector creator
+        total_vehicles_charging = np.zeros([24])
+        for hour in range(24):
+            total_vehicles_charging[hour] = np.sum(charger_occupancy[:, hour])
+
+        self.vehicle_state_of_charge = vehicle_state_of_charge
+        self.arrivals = arrivals
+        self.departures = departures
+        self.charger_occupancy = charger_occupancy
+        self.total_vehicles_charging = total_vehicles_charging
+
+        generated_initial_values = {
+            'SOC': vehicle_state_of_charge,
+            'Arrivals': arrivals,
+            'Departures': departures,
+            'Total vehicles charging': total_vehicles_charging,
+            'Charger occupancy': charger_occupancy
+        }
+
+        savemat(file_directory_path + '\\initial_values.mat', generated_initial_values)
