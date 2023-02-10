2b7d81b89b2c3d343032f3cf609e43304ca8554c
AJ <asolic00@fesb.hr>
2023-02-10 14:35:55 +0100

    Feat: Implement functions from station_simulation in ChargingStation

--- smart_nanogrid_gym/utils/charging_station.py
+++ smart_nanogrid_gym/utils/charging_station.py
@@ -1,3 +1,63 @@
 class ChargingStation:
     def __init__(self, number_of_chargers):
         self.NUMBER_OF_CHARGERS = number_of_chargers
+        self.vehicle_state_of_charge = None
+        self.arrivals = None
+        self.departures = None
+        self.charger_occupancy = None
+        self.departing_vehicles = []
+        self.departure_times = []
+        self.vehicle_state_of_charge_at_current_timestep = []
+
+    def simulate(self, current_timestep):
+        self.find_departing_vehicles(current_timestep)
+        self.calculate_departure_times(current_timestep)
+        self.calculate_state_of_charge_for_each_vehicle(current_timestep)
+
+        return self.departing_vehicles, self.departure_times, self.vehicle_state_of_charge_at_current_timestep
+
+    def find_departing_vehicles(self, hour):
+        if hour >= 24:
+            return []
+
+        self.departing_vehicles.clear()
+        for charger in range(self.NUMBER_OF_CHARGERS):
+            charger_occupied = self.check_charger_occupancy(self.charger_occupancy[charger, hour])
+            vehicle_departing = self.check_is_vehicle_departing(self.departures[charger], hour)
+
+            if charger_occupied and vehicle_departing:
+                self.departing_vehicles.append(charger)
+
+    def check_charger_occupancy(self, charger_occupancy):
+        if charger_occupancy == 1:
+            return True
+        else:
+            return False
+
+    def check_is_vehicle_departing(self, vehicle_departure, hour):
+        if hour + 1 in vehicle_departure:
+            return True
+        else:
+            return False
+
+    def calculate_departure_times(self, hour):
+        self.departure_times.clear()
+        for charger in range(self.NUMBER_OF_CHARGERS):
+            charger_occupied = self.check_charger_occupancy(self.charger_occupancy[charger, hour])
+
+            if charger_occupied:
+                departure_time = self.calculate_next_departure_time(self.departures[charger], hour)
+                self.departure_times.append(departure_time)
+            else:
+                self.departure_times.append(0)
+
+    def calculate_next_departure_time(self, charger_departures, hour):
+        for vehicle in range(len(charger_departures)):
+            if hour <= charger_departures[vehicle]:
+                return charger_departures[vehicle] - hour
+        return []
+
+    def calculate_state_of_charge_for_each_vehicle(self, hour):
+        self.vehicle_state_of_charge_at_current_timestep.clear()
+        for charger in range(self.NUMBER_OF_CHARGERS):
+            self.vehicle_state_of_charge_at_current_timestep.append(self.vehicle_state_of_charge[charger, hour])
