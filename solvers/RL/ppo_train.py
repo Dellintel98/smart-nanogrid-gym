442fda976d6be6e79f7a0635a76a4bc0791158ad
DellIntel98 <asolic00@fesb.hr>
2023-02-25 19:07:41 +0100

    Refactor: Remove unnecessary code and comments

--- solvers/RL/ppo_train.py
+++ solvers/RL/ppo_train.py
@@ -1,17 +1,12 @@
-import gym
 import smart_nanogrid_gym
 import argparse
 
-import numpy
-from stable_baselines3 import DDPG
-from stable_baselines3.common.noise import NormalActionNoise
 import gym
 import numpy as np
 import os
 
-from stable_baselines3 import PPO
 from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
-from stable_baselines3 import DDPG, PPO
+from stable_baselines3 import PPO
 from stable_baselines3.common.evaluation import evaluate_policy
 import time
 
@@ -45,7 +40,7 @@ env_variants = [
             'battery_system_available_in_model': True
         }}
 ]
-current_env = env_variants[0]
+current_env = env_variants[1]
 current_env_name = current_env['variant_name']
 
 models_dir = f"models/PPO-{current_env_name}{int(time.time())}"
@@ -65,7 +60,6 @@ n_actions = env.action_space.shape[-1]
 param_noise = None
 action_noise = OrnsteinUhlenbeckActionNoise(mean=np.zeros(n_actions), sigma=float(0.5) * np.ones(n_actions))
 
-# model = DDPG(MlpPolicy, env, verbose=1, action_noise=action_noise, tensorboard_log=logdir)
 model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=logdir)
 
 TIMESTEPS = 20000
@@ -74,12 +68,10 @@ for i in range(1, 50):
     model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name="PPO")
     model.save(f"{models_dir}/{TIMESTEPS * i}")
 
-
-
 env.close
-#del model # remove to demonstrate saving and loading
+# del model # remove to demonstrate saving and loading
 
-# model = DDPG.load("ddpg_Chargym", env=env)
+# model = DDPG.load("ddpg_gym", env=env)
 #
 # mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
 #
@@ -89,7 +81,3 @@ env.close
 #     action, _states = model.predict(obs, deterministic=True)
 #     obs, rewards, dones, info = env.step(action)
 #     # env.render(
-
-
-
-#aaaaa=1
